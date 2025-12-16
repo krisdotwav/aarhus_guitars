24830fa26d8588b9c1d3be9de8b6bf5b1b473631
NBJ1209 <eaa25nbj@students.eaaa.dk>
2025-12-16 14:58:47 +0100

    Contact pop-up

--- kontakt.js
+++ kontakt.js
@@ -1,64 +1,7 @@
-function showPopupForm() {
-  document.getElementById("popup-form-container").style.display = "block";
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
 }
 
-function hidePopupForm() {
-  document.getElementById("popup-form-container").style.display = "none";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  document.getElementById('show-popup').addEventListener('click', () => {
-    showPopupForm();
-  });
-
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') hidePopupForm()
-  });
-
-});
-
-document.getElementById("contact-form").addEventListener("submit", (event) => {
-  const contactForm = event.target
-  if (!validateContactForm(contactForm)) {
-    event.preventDefault();
-    displayError(contactForm, 'Invalid input')
-  }
-});
-
-function isValidEmail(email) {
-  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  
-  return emailRegex.test(email);
-}
-
-function isValidPhoneNumber(phone) {
-  const phoneRegex = /^\d{8}$/;
-  
-  return phoneRegex.test(phone);
-}
-
-function validateContactForm(contactForm) {
-  const name = contactForm["name"].value;
-  const email = contactForm["email"].value;
-  const phone = contactForm["phone"].value;
-  const message = contactForm["message"].value;
-
-  if (!name || !email || !message) {
-    return false;
-  }
-
-  if (!isValidEmail(email) || (phone && !isValidPhoneNumber(phone))) {
-    return false;
-  }
-
-  return true;
-}
-
-function displayError(formElement, message) {
-  const errorElement = formElement.getElementsByClassName("form-error")[0];
-  
-  errorElement.innerHTML = message;
-  
-  errorElement.style.display = "block";
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
